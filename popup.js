e8e2fed019ca026b31b9f6340a7c9b328068cf09
Miguel Gomes Lopes <miguel.gomes.lopes@outsystems.com>
2025-01-03 13:30:21 +0000

    linted

--- popup.js
+++ popup.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   const starRatingSlider = document.getElementById('starRating');
   const starRatingNumber = document.getElementById('starRatingNumber');
-  const reviewNumber= document.getElementById('reviewCountNumber')
-  const maxPriceNumber= document.getElementById('maxPriceNumber')
+  const reviewNumber = document.getElementById('reviewCountNumber')
+  const maxPriceNumber = document.getElementById('maxPriceNumber')
   const freeDeliveryOnlyCheckbox = document.getElementById('freeDeliveryOnly');
   const primeOnlyCheckbox = document.getElementById('primeOnly');
   const applyFilterButton = document.getElementById('applyFilter');
 
   // Retrieve stored filter values and set them as initial values
-  chrome.storage.sync.get(['starRating', 'reviewNumber', 'maxPrice', 'freeDelivery', 'prime'], function(items) {
+  chrome.storage.sync.get(['starRating', 'reviewNumber', 'maxPrice', 'freeDelivery', 'prime'], function (items) {
     starRatingSlider.value = items.starRating || starRatingNumber.value;
     starRatingNumber.value = items.starRating || starRatingNumber.value;
     reviewNumber.value = items.reviewNumber || reviewNumber.value;
@@ -18,18 +18,18 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // link inputs and save filter values to storage
-  starRatingSlider.addEventListener('input', function() {
+  starRatingSlider.addEventListener('input', function () {
     starRatingNumber.value = this.value;
     chrome.storage.sync.set({ 'starRating': this.value });
   });
-  starRatingNumber.addEventListener('input', function() {
+  starRatingNumber.addEventListener('input', function () {
     starRatingSlider.value = this.value;
     chrome.storage.sync.set({ 'starRating': this.value });
   });
-  reviewNumber.addEventListener('input', function() {
+  reviewNumber.addEventListener('input', function () {
     chrome.storage.sync.set({ 'reviewNumber': this.value });
   });
-  maxPriceNumber.addEventListener('input', function() {
+  maxPriceNumber.addEventListener('input', function () {
     chrome.storage.sync.set({ 'maxPrice': this.value });
   });
   freeDeliveryOnlyCheckbox.addEventListener('change', function () {
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function applyFilter(starRatingThreshold, reviewNumberThreshold, priceThreshold, freeDeliveryOnly, primeOnly) {
 
 
-  function parseStringAsNumber(text){
+  function parseStringAsNumber(text) {
     // get only the first block of numeric, comma and dot characters
     let cleanedString = text.match(/[0-9.,]+/)[0];
 
@@ -72,17 +72,17 @@ function applyFilter(starRatingThreshold, reviewNumberThreshold, priceThreshold,
     const lastDotIndex = cleanedString.lastIndexOf('.');
     const lastCommaIndex = cleanedString.lastIndexOf(',');
     if (lastDotIndex > lastCommaIndex) {
-        cleanedString = cleanedString.replace(',', '');
-        if (cleanedString.length - lastDotIndex > 3) {
-            cleanedString = cleanedString.replace('.', '');
-        }
-    } else if (lastCommaIndex > lastDotIndex) {
+      cleanedString = cleanedString.replace(',', '');
+      if (cleanedString.length - lastDotIndex > 3) {
         cleanedString = cleanedString.replace('.', '');
-        if (cleanedString.length - lastCommaIndex > 3) {
-            cleanedString = cleanedString.replace(',', '');
-        } else {
-            cleanedString = cleanedString.replace(',', '.');
-        }
+      }
+    } else if (lastCommaIndex > lastDotIndex) {
+      cleanedString = cleanedString.replace('.', '');
+      if (cleanedString.length - lastCommaIndex > 3) {
+        cleanedString = cleanedString.replace(',', '');
+      } else {
+        cleanedString = cleanedString.replace(',', '.');
+      }
     }
 
     // Parse as float and return the result
@@ -90,7 +90,7 @@ function applyFilter(starRatingThreshold, reviewNumberThreshold, priceThreshold,
   }
 
   const divs = document.querySelectorAll('[data-component-type="s-search-result"]');
-  for(let div of divs){
+  for (let div of divs) {
 
     // define relevant parent divs
     const deliveryDiv = div.querySelector('[data-cy="delivery-recipe"]')
@@ -104,16 +104,16 @@ function applyFilter(starRatingThreshold, reviewNumberThreshold, priceThreshold,
     let price = 0;
     let prime;
     let freeDelivery;
-    if(deliveryDiv){
+    if (deliveryDiv) {
       prime = deliveryDiv.querySelector('[aria-label="Amazon Prime"]');
       freeDelivery = deliveryDiv.querySelector('[aria-label^="Entrega GRATIS"]');
     }
     try {
-      if(starReviewDiv){
+      if (starReviewDiv) {
         starRating = parseStringAsNumber(starReviewDiv.querySelector('[aria-label$="estrellas"]').textContent);
         reviewNumber = parseStringAsNumber(starReviewDiv.querySelector('span.a-size-base.s-underline-text').textContent);
       }
-      if(priceDiv){
+      if (priceDiv) {
         price = parseStringAsNumber(priceDiv.querySelector('.a-price').textContent);
       }
     } catch {
@@ -122,10 +122,10 @@ function applyFilter(starRatingThreshold, reviewNumberThreshold, priceThreshold,
     }
 
     // filter values
-    if (!prime && primeOnly){
+    if (!prime && primeOnly) {
       div.remove();
     }
-    else if (!freeDelivery && freeDeliveryOnly){
+    else if (!freeDelivery && freeDeliveryOnly) {
       div.remove();
     }
     else if (starRating < starRatingThreshold) {
